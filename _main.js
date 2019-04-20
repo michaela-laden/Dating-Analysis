30274cd5a62fa7b08cf3bffb7b753dcb7485c94d
ladenm <42896556+ladenm@users.noreply.github.com>
2019-04-20 13:14:51 -0500

    Add files via upload

---
+++ _main.js
@@ -0,0 +1,14 @@
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
+
+
+
+
+
